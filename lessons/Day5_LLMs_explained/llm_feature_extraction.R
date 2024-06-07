9fadc895b48ce6e42475f3b5b406d58edc9c1641
Kwartler <you@example.com>
2024-06-06 20:50:02 -0400

    move day 5 up to 4

--- lessons/Day5_LLMs_explained/llm_feature_extraction.R
+++
@@ -1,8 +0,0 @@
-#' Title: Intro: LLM Based Sentiment
-#' Purpose: Feature Extraction & Sentiment using a local LLM
-#' Author: Ted Kwartler
-#' Date: May 12, 2024
-#' Source: Clinton Emails
-
-
-# End
